9fa1e5dc7e1394472e9ca7ff27c62fba1b628b62
OmegaChi <88118322+OmegaChi@users.noreply.github.com>
2022-02-07 16:05:34 -0600

    Create StarTriangle.java

---
+++ chapter-2/StarTriangle.java
@@ -0,0 +1,13 @@
+public class StarTriangle {
+  
+    public static void main(String[] args) {
+      
+        for (int layers = 1; layers <= 5; layers++) {
+            for (int asterisk = 1; asterisk <= layers; asterisk++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+      
+   }
+}
