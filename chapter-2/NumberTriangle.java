8ab6789c140b99754847f7b8fdc6888c9c178802
OmegaChi <88118322+OmegaChi@users.noreply.github.com>
2022-02-10 19:32:49 -0600

    Create NumberTriangle.java

---
+++ chapter-2/NumberTriangle.java
@@ -0,0 +1,28 @@
+/*
+Write for loops to produce the following output:
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+*/
+
+public class NumberTriangle {
+
+  public static void main(String[] args) {
+  
+      final int MAX_NUM_OF_LAYERS = 7;
+      int numberPyramid = 1;
+      
+      for (int layer = 1; layer <= MAX_NUM_OF_LAYERS; layer++) {
+          for (int i = 1; i <= numberPyramid; i++) {
+              System.out.print(numberPyramid);
+          }
+          numberPyramid++;
+          System.out.println();
+      }
+  }
+}
